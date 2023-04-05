87573df4fdb503d562058c48c7823cacaca0c329
kylerasinger <kyle.rasinger@gmail.com>
2023-04-05 15:47:19 -0400

    read input file

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <algorithm>
 #include "Ta.h"
 
 using namespace std;
@@ -9,6 +13,22 @@ int main()
     cout << "Program Start: \n" << endl;
     vector<Ta>* taList = new vector<Ta>;
 
+    ifstream taListFile("ta_list.txt");
+    ostringstream ss;
+    string line;
+    int currentLine = 0;
+
+    while(!taListFile.eof())
+    {
+        currentLine++;
+        getline(taListFile, line);
+        ss << line;
+        ss << "\n";
+    }
+    cout << ss.str();
+
+
+    /*
     Ta firstTa("BCEE", "Alum", 2016, 9886150);
     Ta secondTa("MIAE", "Grad", 2017, 8829755);
 
@@ -17,6 +37,6 @@ int main()
 
     cout << "vector1 dept: " << (*taList)[0].getDept() << "\n";
     cout << "vector2 dept: " << (*taList)[1].getDept() << "\n";
-
+    */
     return 0;
 }
